628a0e406bb273bc97c164ed78e28f6e6c880d8d
Henke <cyt137@dca90475ece9.cof.ds.capitalone.com>
2017-07-19 15:22:40 -0400

    finished basic game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,7 +32,8 @@ var checkForMatch = function(){
   }
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
 
   cardsInPlay.push(cards[cardId].rank);
@@ -40,10 +41,23 @@ var flipCard = function(cardId){
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
 
+  this.setAttribute('src',cards[cardId].cardImage);
+
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++){
+    //add cards[i] to the board
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src',"images/back.png");
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+
+
+createBoard();
